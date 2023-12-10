a7c43af11dc2553528d0d37ce0258f7e98139c9a
Mexion <dev@mexion.com>
2023-12-10 16:20:29 +0100

    Add rule translation

---
+++ src/Rules/BaseValidationRule.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Yormy\ValidationLaravel\Rules;
+
+use Exception;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+abstract class BaseValidationRule implements ValidationRule
+{
+    public function __construct(private ?Exception $exception = null)
+    {
+        // ...
+    }
+}
--- src/Rules/EmailNotBanned.php
+++ src/Rules/EmailNotBanned.php
@@ -1,15 +1,11 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Yormy\ValidationLaravel\Rules;
 
 use Closure;
-use Illuminate\Contracts\Validation\ValidationRule;
-use Mexion\BedrockUsersv2\Domain\User\Repositories\MemberRepository;
 use Yormy\ValidationLaravel\Facades\BannedEmail;
 
-class EmailNotBanned implements ValidationRule
+class EmailNotBanned extends BaseValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
