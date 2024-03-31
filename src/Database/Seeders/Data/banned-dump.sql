9bc396916158287d2781531101583f9d182a1230
RobPri <rob@prigroup.eu>
2024-03-31 19:32:16 +0200

    fix dumped import

--- src/Database/Seeders/Data/banned-dump.sql
+++ src/Database/Seeders/Data/banned-dump.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `banned_emails`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `banned_emails` (
-  `id` bigint unsigned NOT NULL /*!40101 AUTO_INCREMENT */,,
+  `id` bigint unsigned NOT NULL /*!40101 AUTO_INCREMENT */,
   `banned` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `notes` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
