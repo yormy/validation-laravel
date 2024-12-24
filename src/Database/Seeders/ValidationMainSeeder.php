5f0b549a767d61454164e3755bb61c81005f204e
Mexion <dev@mexion.com>
2024-12-24 13:54:47 +0100

    add banned emails seeder

--- src/Database/Seeders/ValidationMainSeeder.php
+++ src/Database/Seeders/ValidationMainSeeder.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Yormy\ValidationLaravel\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Yormy\ValidationLaravel\Models\BannedEmail;
 
 class ValidationMainSeeder extends Seeder
 {
     public function run(): void
     {
-        // ...
+        BannedEmail::create(['email' => 'banned-domain.com']);
+        BannedEmail::create(['email' => 'banned@banned-email.com']);
     }
 }
--- src/Rules/EmailNotBanned.php
+++ src/Rules/EmailNotBanned.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Yormy\ValidationLaravel\Rules;
 
 use Closure;
-use Yormy\ValidationLaravel\Facades\DisposableEmail;
+use Yormy\ValidationLaravel\Facades\BannedEmail;
 
 class EmailNotBanned extends BaseValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (DisposableEmail::isDisposable($value)) {
+        if (BannedEmail::isBanned($value)) {
             $fail(__('validation::rule.email.banned', ['attribute' => $attribute, 'value' => $value]));
         }
     }
